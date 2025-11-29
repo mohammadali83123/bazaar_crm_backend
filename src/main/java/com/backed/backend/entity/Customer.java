19c839576a3741aefe448a15317c5c7c95fcf750
mohammadali83123 <mohammadali83123@gmail.com>
2025-11-29 16:47:35 +0500

    added fields to customer entity with id, name, channel and creation timestamp

--- src/main/java/com/backed/backend/entity/Customer.java
+++ src/main/java/com/backed/backend/entity/Customer.java
@@ -1,9 +1,21 @@
 package com.backed.backend.entity;
 
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
 
 @Data
 @Document(collection = "customer")
 public class Customer {
+    @Id
+    private String customerId;
+    private String customerName;
+    private String channelId;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
