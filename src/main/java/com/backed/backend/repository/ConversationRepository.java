5a17b229926c20f55391fc2088c30442fd736a13
mohammadali83123 <mohammadali83123@gmail.com>
2025-11-29 16:31:17 +0500

    added missing MongoRepository import to conversation and message repositories

--- src/main/java/com/backed/backend/repository/ConversationRepository.java
+++ src/main/java/com/backed/backend/repository/ConversationRepository.java
@@ -1,8 +1,9 @@
 package com.backed.backend.repository;
 
 import com.backed.backend.entity.Conversation;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConversationRepository extends MongoRepository<Conversation, Object>{
+public interface ConversationRepository extends MongoRepository<Conversation, Object> {
 }
--- src/main/java/com/backed/backend/repository/MessageRepository.java
+++ src/main/java/com/backed/backend/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.backed.backend.repository;
 
 import com.backed.backend.entity.Message;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
